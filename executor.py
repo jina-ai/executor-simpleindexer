34cdfce3e5b8ecdb34557fd4e1e67732b123bbaa
Johannes Messner <messnerjo@gmail.com>
2022-01-26 11:34:32 +0100

    fix: migrate to Jina 3, remove un-simple features

--- executor.py
+++ executor.py
@@ -1,25 +1,29 @@
 import inspect
 from copy import deepcopy
 from typing import Dict, Optional
+import os
+import uuid
 
-from jina import DocumentArray, Executor, requests
+from jina import DocumentArray, Document, Executor, requests
 from jina.logging.logger import JinaLogger
-from jina import DocumentArrayMemmap
+
 
 
 class SimpleIndexer(Executor):
     """
-    A simple indexer that stores all the Document data together,
-    in a DocumentArrayMemmap object
+    A simple indexer that stores all the Document data together in a DocumentArray,
+    and can dump to and load from disk.
 
     To be used as a unified indexer, combining both indexing and searching
     """
 
+    FILE_NAME = 'index.bin'
+
     def __init__(
         self,
         match_args: Optional[Dict] = None,
-        key_length: int = 64,
-        buffer_pool_size: int = 100_000,
+        protocol: str = 'pickle-array',
+        compress: str = None,
         **kwargs,
     ):
         """
@@ -29,6 +33,8 @@ class SimpleIndexer(Executor):
         :param match_args: the arguments to `DocumentArray`'s match function
         :param key_length: the `key_length` keyword argument to
         `DocumentArrayMemmap`'s constructor
+        :param protocol: serialisation protocol for disk access
+        :param compress: compression algorithm for disk access
         :param buffer_pool_size: the `buffer_pool_size` argument to
         `DocumentArrayMemmap`'s constructuor., which stores
             the indexed Documents. During querying, the embeddings of the indexed
@@ -43,11 +49,17 @@ class SimpleIndexer(Executor):
         """
         super().__init__(**kwargs)
 
+        self.protocol = protocol
+        self.compress = compress
         self._match_args = match_args or {}
-        self._storage = DocumentArrayMemmap(
-            self.workspace, key_length=key_length, buffer_pool_size=buffer_pool_size
-        )
+        self._index = DocumentArray()
         self.logger = JinaLogger(self.metas.name)
+        try:
+            self.load_from_disk()
+        except FileNotFoundError:
+            self.logger.info(
+                f'no data found in the workspace'
+            )
 
     @requests(on='/index')
     def index(
@@ -59,7 +71,7 @@ class SimpleIndexer(Executor):
         :param docs: the docs to add
         """
         if docs:
-            self._storage.extend(docs)
+            self._index.extend(docs)
 
     @requests(on='/search')
     def search(
@@ -76,36 +88,18 @@ class SimpleIndexer(Executor):
         """
         if not docs:
             return
-        match_args = deepcopy(self._match_args)
-        if parameters:
-            match_args.update(parameters)
-
-        match_args = SimpleIndexer._filter_parameters(docs, match_args)
-        left_trav_path = match_args.pop('traversal_ldarray', None)
-        # if it's 'r' we would just be duplicating
-        if left_trav_path and left_trav_path != 'r':
-            left_trav_docs = DocumentArray(docs.traverse_flat(left_trav_path))
-        else:
-            left_trav_docs = docs
-
-        right_trav_path = match_args.pop('traversal_rdarray', None)
-        # if it's 'r' we would just be duplicating
-        if right_trav_path and right_trav_path != 'r':
-            right_trav_docs = DocumentArray(
-                self._storage.traverse_flat(right_trav_path)
-            )
-        else:
-            right_trav_docs = self._storage
-
-        left_trav_docs.match(right_trav_docs, **match_args)
+        match_args = {**self._match_args, **parameters} if parameters is not None else self._match_args
+        match_args = SimpleIndexer._filter_match_params(docs, match_args)
+        docs.match(self._index, **match_args)
 
     @staticmethod
-    def _filter_parameters(docs, match_args):
+    def _filter_match_params(docs, match_args):
         # get only those arguments that exist in .match
         args = set(inspect.getfullargspec(docs.match).args)
         args.discard('self')
         match_args = {k: v for k, v in match_args.items() if k in args}
         return match_args
+
 
     @requests(on='/delete')
     def delete(self, parameters: Dict, **kwargs):
@@ -114,10 +108,9 @@ class SimpleIndexer(Executor):
         :param parameters: parameters to the request
         """
         deleted_ids = parameters.get('ids', [])
-
-        for idx in deleted_ids:
-            if idx in self._storage:
-                del self._storage[idx]
+        if len(deleted_ids) == 0:
+            return
+        del self._index[deleted_ids]
 
     @requests(on='/update')
     def update(self, docs: Optional[DocumentArray], **kwargs):
@@ -131,7 +124,7 @@ class SimpleIndexer(Executor):
 
         for doc in docs:
             try:
-                self._storage[doc.id] = doc
+                self._index[doc.id] = doc
             except IndexError:
                 self.logger.warning(
                     f'cannot update doc {doc.id} as it does not exist in storage'
@@ -147,4 +140,22 @@ class SimpleIndexer(Executor):
             return
 
         for doc in docs:
-            doc.embedding = self._storage[doc.id].embedding
+            doc.embedding = self._index[doc.id].embedding
+
+    @requests(on='/dump')
+    def dump(self, docs: Optional[DocumentArray]=None, **kwargs):
+        """dump indexed Documents to disk
+        """
+        if docs:
+            self._index.extend(docs)
+        bytes = self._index.to_bytes(protocol=self.protocol, compress=self.compress)
+        with open(os.path.join(self.workspace, SimpleIndexer.FILE_NAME), 'wb') as f:
+            f.write(bytes)
+
+    @requests(on='/load')
+    def load_from_disk(self, docs: Optional[DocumentArray]=None, **kwargs):
+        with open(os.path.join(self.workspace, SimpleIndexer.FILE_NAME), 'rb') as f:
+            self._index = DocumentArray.from_bytes(f, protocol=self.protocol, compress=self.compress)
+        if docs:
+            self._index.extend(docs)
+
